e4ce6c98be0e9e4934c2cf01fefb79ee6968b975
Anas Ambri <anas.ambri@gmail.com>
2013-07-13 20:22:48 -0400

    Adding auto-complete mode and .gitignore file

--- init.el
+++ init.el
@@ -1,5 +1,17 @@
+;No files with annoying tildes, even if it means no backup
 (setq make-backup-files nil)
+
+;magit
 (add-to-list 'load-path "~/.emacs.d/modes/magit")
-(add-to-list 'load-path "~/.emacs.d/modes/nodejs")
 (require 'magit)
-(require 'nodejs-repl)
+
+;auto-complete based off
+; http://blog.deadpansincerity.com/2011/05/setting-up-emacs-as-a-javascript-editing-environment-for-fun-and-profit/
+(add-to-list 'load-path "~/.emacs.d/modes/auto-complete")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/dict")
+(setq-default ac-sources (add-to-list 'ac-sources 'ac-source-dictionary))
+(global-auto-complete-mode t)
+(setq ac-auto-start 2)
+;ignore case when auto-completing
+(setq ac-ignore-case t)
