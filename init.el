5f5e000d835c3b685ddf510e917683475ab55ed0
Anas Ambri <anas.ambri@gmail.com>
2014-01-05 12:06:09 -0500

    Adding line/column-mode, adding uniquify

--- init.el
+++ init.el
@@ -1,14 +1,12 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Config
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;No files with annoying tildes, even if it means no backup
 (setq make-backup-files nil)
 
 ;Copy & paste from clipboard
 (setq x-select-enable-clipboard t)
-
-;Window resizing
-(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
-(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
-(global-set-key (kbd "S-C-<down>") 'shrink-window)
-(global-set-key (kbd "S-C-<up>") 'enlarge-window)
 
 ;EJS files use html-mode
 (add-to-list 'auto-mode-alist '("\\.ejs\\'" . html-mode))
@@ -17,47 +15,31 @@
 ;JS files use js2-mode
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
-;magit
-;;;;;;
-(add-to-list 'load-path "~/.emacs.d/plugins/magit")
-(require 'magit)
+;uniquify
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward
+      uniquify-separator ":")
+
+;column and line-mode
+(setq line-number-mode t)
+(setq column-number-mode t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Hacks
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;Window resizing
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
 
 
-;nodejs-repl
-;;;;;;;;;;;;
-(add-to-list 'load-path "~/.emacs.d/plugins/nodejs")
-(require 'nodejs-repl)
 
 
-; auto-complete and yasnippet based off
-; http://blog.deadpansincerity.com/2011/05/setting-up-emacs-as-a-javascript-editing-environment-for-fun-and-profit/
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(add-to-list 'load-path "~/.emacs.d/plugins/auto-complete")
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/plugins/auto-complete/dict")
-(setq-default ac-sources (add-to-list 'ac-sources 'ac-source-dictionary))
-(global-auto-complete-mode t)
-(setq ac-auto-start 1)
-;ignore case when auto-completing
-(setq ac-ignore-case t)
-
-
-;Snippeting
-;;;;;;;;;;;
-(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
-(require 'yasnippet)
-(yas/global-mode t)
-;Taken from http://stackoverflow.com/questions/15774807/emacs-24-autocomplete-yasnippet
-(setq ac-source-yasnippet nil)
-(setq yas-snippet-dirs "~/.emacs.d/plugins/yasnippet/snippets")
-
-
-;Adding eproject
-;;;;;;;;;;;;;;;;
-;(add-to-list 'load-path "~/.emacs.d/plugins/eproject")
-;(require 'eproject)
-;(require 'eproject-extras)
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Modes
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;Ergoemacs : http://ergoemacs.github.io/ergoemacs-mode/
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -70,36 +52,6 @@
 (ergoemacs-mode 1)
 ;Custom commands
 (ergoemacs-key "C-\'" 'newline-and-indent "Execute")
-
-
-;Lookup documentation online
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Based off http://ergoemacs.org/emacs/emacs_lookup_ref.html
-(add-to-list 'load-path "~/.emacs.d/plugins/lookup-word")
-(require 'lookup-word)
-(setq lookup-hash (make-hash-table :test 'equal))
-(puthash "node" "http://www.google.com/search?q=nodejs+�" lookup-hash)
-(puthash "nodejs" "http://www.google.com/search?q=nodejs+�" lookup-hash)
-(puthash "juice" "https://github.com/LearnBoost/juice#documentation" lookup-hash)
-
-(defun lookup-mode-specific (site &optional input-word)
-  "Lookup current word or text selection depending on given site."
-  (interactive "sEnter documentation site: ")
-  (lookup-word-on-internet input-word (gethash site lookup-hash) ) )
-
-
-;json-mode
-;;;;;;;;;;;;
-(add-to-list 'load-path "~/.emacs.d/modes/json-mode")
-(require 'json-mode)
-; beautify-json based off http://stackoverflow.com/questions/435847/emacs-mode-to-edit-json#answer-7934783
-(defun beautify-json ()
-  (interactive)
-  (let ((b (if mark-active (min (point) (mark)) (point-min)))
-        (e (if mark-active (max (point) (mark)) (point-max))))
-    (shell-command-on-region b e
-     "python -mjson.tool" (current-buffer) t)))
-
 
 ;Adding android-mode
 ;;;;;;;;;;;;;;;;;;;;
@@ -115,13 +67,11 @@
 (add-hook 'java-mode-hook
       (function (lambda() (java-mode-indent-annotations-setup))))
 
-
 ;arduino-mode
 ;;;;;;;;;;;;;
 (add-to-list 'load-path "~/.emacs.d/modes/arduino-mode")
 (setq auto-mode-alist (cons '("\\.\\(pde\\|ino\\)$" . arduino-mode) auto-mode-alist))
 (autoload 'arduino-mode "arduino-mode" "Arduino editing mode." t)
-
 
 ;octave-mode
 ;;;;;;;;;;;;
@@ -141,3 +91,74 @@
 	      'comint-previous-input)
 	    (define-key inferior-octave-mode-map [down]
 	      'comint-next-input))) 
+
+;json-mode
+;;;;;;;;;;;;
+(add-to-list 'load-path "~/.emacs.d/modes/json-mode")
+(require 'json-mode)
+; beautify-json based off http://stackoverflow.com/questions/435847/emacs-mode-to-edit-json#answer-7934783
+(defun beautify-json ()
+  (interactive)
+  (let ((b (if mark-active (min (point) (mark)) (point-min)))
+        (e (if mark-active (max (point) (mark)) (point-max))))
+    (shell-command-on-region b e
+     "python -mjson.tool" (current-buffer) t)))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Plugins
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;magit
+;;;;;;
+(add-to-list 'load-path "~/.emacs.d/plugins/magit")
+(require 'magit)
+
+;nodejs-repl
+;;;;;;;;;;;;
+(add-to-list 'load-path "~/.emacs.d/plugins/nodejs")
+(require 'nodejs-repl)
+
+; auto-complete and yasnippet based off
+; http://blog.deadpansincerity.com/2011/05/setting-up-emacs-as-a-javascript-editing-environment-for-fun-and-profit/
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-to-list 'load-path "~/.emacs.d/plugins/auto-complete")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/plugins/auto-complete/dict")
+(setq-default ac-sources (add-to-list 'ac-sources 'ac-source-dictionary))
+(global-auto-complete-mode t)
+(setq ac-auto-start 1)
+;ignore case when auto-completing
+(setq ac-ignore-case t)
+
+;Snippeting
+;;;;;;;;;;;
+(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet)
+(yas/global-mode t)
+;Taken from http://stackoverflow.com/questions/15774807/emacs-24-autocomplete-yasnippet
+(setq ac-source-yasnippet nil)
+(setq yas-snippet-dirs "~/.emacs.d/plugins/yasnippet/snippets")
+
+;Adding eproject
+;;;;;;;;;;;;;;;;
+;(add-to-list 'load-path "~/.emacs.d/plugins/eproject")
+;(require 'eproject)
+;(require 'eproject-extras)
+
+;Lookup documentation online
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Based off http://ergoemacs.org/emacs/emacs_lookup_ref.html
+(add-to-list 'load-path "~/.emacs.d/plugins/lookup-word")
+(require 'lookup-word)
+(setq lookup-hash (make-hash-table :test 'equal))
+(puthash "node" "http://www.google.com/search?q=nodejs+�" lookup-hash)
+(puthash "nodejs" "http://www.google.com/search?q=nodejs+�" lookup-hash)
+(puthash "juice" "https://github.com/LearnBoost/juice#documentation" lookup-hash)
+
+(defun lookup-mode-specific (site &optional input-word)
+  "Lookup current word or text selection depending on given site."
+  (interactive "sEnter documentation site: ")
+  (lookup-word-on-internet input-word (gethash site lookup-hash) ) )
+
